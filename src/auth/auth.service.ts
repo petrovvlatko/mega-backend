e6648431ab0a1f21946ccc720b96ca56ddbc6ecf
V.Petrov <petrovvlatko93@gmail.com>
2023-09-16 23:24:38 -0400

    adding auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  //  MAKE SURE TO USE bcrypt WHEN PERSISTING PASSWORDS TO A DB
+
+  async signIn(username: string, pass: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const { password, ...result } = user;
+    console.log(password);
+    // TODO: Generate a JWT and return it here
+    // instead of the user object
+    return result;
+  }
+}
