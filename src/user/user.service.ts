3466255b6a7fe2ad0b2ba4cd8b9d9b9750db962e
V.Petrov <petrovvlatko93@gmail.com>
2023-09-16 23:17:58 -0400

    commented out unused imports and deleted boilerplate code

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+// import { CreateUserDto } from './dto/create-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
+
+// This should be a real class/interface representing a user entity
+export type User = any;
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  private readonly userList = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findOne(username: string): Promise<User | undefined> {
+    return this.userList.find((user) => user.username === username);
   }
 }
