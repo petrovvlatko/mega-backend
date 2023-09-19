856407d0afa8fbceaa0cdc9501ad3156bdd8d296
V.Petrov <petrovvlatko93@gmail.com>
2023-09-19 16:23:55 -0400

    global auth set up and examples shown in user controller and user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,10 +8,17 @@ import {
   // Delete,
 } from '@nestjs/common';
 import { UserService } from './user.service';
+// import { SkipAuth } from 'src/auth/decorators/skipAuth.decorator';
 // import { CreateUserDto } from './dto/create-user.dto';
 // import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+  // This is an example of how to use SkipAuth()
+  // @SkipAuth()
+  // @Get()
+  // sendMessage() {
+  //   return { message: 'User Auth Test' };
+  // }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    UserService,
+    // The following object is necessary in order to import and use the custom SkipAuth() decorator
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   exports: [UserService],
 })
 export class UserModule {}
