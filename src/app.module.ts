456adb55017e9e8db9c207823911c723168e77bc
V.Petrov <petrovvlatko93@gmail.com>
2023-09-20 20:52:21 -0400

    removed unused modules ... adding back in the future but for now attempting to finish db connection with auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RoomModule } from './room/room.module';
-import { TagModule } from './tag/tag.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [RoomModule, TagModule, UserModule, AuthModule, ItemModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
