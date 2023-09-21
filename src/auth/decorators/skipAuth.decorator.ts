835c5a217896d61ef1bee33752a2d6fe28a7f6b8
V.Petrov <petrovvlatko93@gmail.com>
2023-09-20 20:55:32 -0400

    updated secrets

--- src/auth/decorators/skipAuth.decorator.ts
+++ src/auth/decorators/skipAuth.decorator.ts
@@ -1,4 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const SKIP_AUTH_KEY = 'skipAuthentication';
+export const SKIP_AUTH_KEY =
+  'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.';
 export const SkipAuth = () => SetMetadata(SKIP_AUTH_KEY, true);
