d3b9f3b76a1c4a29ccd0aeb0b0d8a9e51a8e378f
V.Petrov <petrovvlatko93@gmail.com>
2023-10-15 12:40:31 -0400

    all set, migrations correct, need to refactor user entity, controller, service, dto to users

--- src/migrations/1697386288899-users.ts
+++ src/migrations/1697386288899-users.ts
@@ -4,12 +4,10 @@ export class Users1697386288899 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(
       `CREATE TABLE "users" (
-        "id" SERIAL NOT NULL, 
-        "username" character varying NOT NULL, 
-        "password" character varying NOT NULL, 
-        "userType" character varying NOT NULL DEFAULT 'user',
-        CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" 
-        PRIMARY KEY ("id"))`,
+        "id" SERIAL NOT NULL PRIMARY KEY,
+        "username" character varying NOT NULL,
+        "password" character varying NOT NULL,
+        "userType" character varying NOT NULL DEFAULT 'user')`,
     );
   }
 
