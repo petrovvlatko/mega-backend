3138ff6241e907102dc96bab9c28ca947a972a11
V.Petrov <petrovvlatko93@gmail.com>
2023-10-25 14:54:39 -0400

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,8 @@ async function bootstrap() {
     }),
   );
   app.use(cookieParser());
+  // Remember to set specific sites for CORS to ensure that only specific sites can access!!!
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
