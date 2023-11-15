26d9ceb50730811ed649a81cbe06d3534dae56c3
V.Petrov <petrovvlatko93@gmail.com>
2023-11-14 21:39:52 -0500

    added PORT variable

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
@@ -36,6 +38,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
