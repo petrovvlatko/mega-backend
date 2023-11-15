fdcacc59ad0d85092e8107e9a42dbf00221efd8d
V.Petrov <petrovvlatko93@gmail.com>
2023-11-14 20:23:23 -0500

    changed message and view for /

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 import { SkipAuth } from './auth/decorators/skipAuth.decorator';
 
@@ -8,6 +8,7 @@ export class AppController {
 
   @SkipAuth()
   @Get()
+  @Render('index')
   getIntroMessage(): string {
     return this.appService.getIntroMessage();
   }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getIntroMessage(): string {
-    return `<h1>Welcome to James' NestJS Api!</h1><p>This is a boilerplate for a NestJS API with JWT authentication and PostgreSQL database integration.</p>`;
+  getIntroMessage(): any {
+    return { message: 'Hello World!' };
   }
 }
