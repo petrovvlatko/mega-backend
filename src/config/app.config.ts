1f3996133953a7059aecdfd02a6964dd9949a714
V.Petrov <petrovvlatko93@gmail.com>
2023-11-14 20:49:58 -0500

    updated app config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -4,11 +4,7 @@ export default registerAs('app', () => ({
   environment: process.env.ENVIRONMENT,
   database: {
     type: 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: +process.env.DATABASE_PORT || 5432,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    url: process.env.DATABASE_URL,
     autoLoadEntities: true,
   },
 }));
