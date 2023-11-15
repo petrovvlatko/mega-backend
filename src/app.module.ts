a598914f7416c1a2ae6bdaf6aad05a4a23a053f5
V.Petrov <petrovvlatko93@gmail.com>
2023-11-14 20:31:10 -0500

    added home view

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,11 +32,6 @@ import appConfig from './config/app.config';
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'postgres',
-        // host: process.env.DATABASE_HOST,
-        // port: +process.env.DATABASE_PORT,
-        // username: process.env.DATABASE_USERNAME,
-        // password: process.env.DATABASE_PASSWORD,
-        // database: process.env.DATABASE_NAME,
         url: process.env.DATABASE_URL,
         autoLoadEntities: true,
       }),
--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -4,11 +4,6 @@ dotenv.config();
 
 export default new DataSource({
   type: 'postgres',
-  // host: process.env.DATABASE_HOST,
-  // port: +process.env.DATABASE_PORT,
-  // username: process.env.DATABASE_USERNAME,
-  // password: process.env.DATABASE_PASSWORD,
-  // database: process.env.DATABASE_NAME,
   url: process.env.DATABASE_URL,
   entities: ['dist/src/**/*.entity.js'],
   migrations: ['dist/src/migrations/*.js'],
