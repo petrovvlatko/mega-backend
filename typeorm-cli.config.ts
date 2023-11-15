fa6210ab12e6c1553103fb58c55544095ab9fa98
V.Petrov <petrovvlatko93@gmail.com>
2023-11-14 19:58:40 -0500

    swapped out database variables for database URL

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -4,11 +4,12 @@ dotenv.config();
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: +process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  // host: process.env.DATABASE_HOST,
+  // port: +process.env.DATABASE_PORT,
+  // username: process.env.DATABASE_USERNAME,
+  // password: process.env.DATABASE_PASSWORD,
+  // database: process.env.DATABASE_NAME,
+  url: process.env.DATABASE_URL,
   entities: ['dist/src/**/*.entity.js'],
   migrations: ['dist/src/migrations/*.js'],
 });
