20276cb055c6219a00eb75bc9209499f3b1250b0
V.Petrov <petrovvlatko93@gmail.com>
2023-11-15 20:51:18 -0500

    updated allowed origins

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
+  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -31,9 +31,7 @@ async function bootstrap() {
   // Remember to ensure that only specific origins can communicate with your API!!!
   app.enableCors({
     allowedHeaders: 'Content-Type, Accept',
-    origin: '*',
-    // Use the following in production (see variable declaration on line 10 above):
-    // origin: allowedOrigins
+    origin: allowedOrigins,
     methods: 'GET,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
