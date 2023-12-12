ef43f60ec4fcda56c73d26750ab4cdca979498b8
V.Petrov <petrovvlatko93@gmail.com>
2023-12-12 07:17:53 -0500

    added Set-Cookie to exposed headers

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,7 @@ async function bootstrap() {
     origin: allowedOrigins,
     methods: 'GET,PUT,PATCH,POST,DELETE',
     credentials: true,
-    exposedHeaders: 'Authorization, Access-Control-Allow-Origin',
+    exposedHeaders: 'Authorization, Access-Control-Allow-Origin, Set-Cookie',
   });
 
   await app.listen(PORT);
