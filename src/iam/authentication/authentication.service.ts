10d86d2172d7c93993abe2980ca4a84b41f5835c
V.Petrov <petrovvlatko93@gmail.com>
2023-12-23 16:03:20 -0500

    set up active user decorator

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -13,6 +13,7 @@ import { SignInDto } from './dto/sign-in.dto';
 import { JwtService } from '@nestjs/jwt';
 import jwtConfig from '../config/jwt.config';
 import { ConfigType } from '@nestjs/config';
+import { ActiveUserData } from '../interfaces/active-user-data.interface';
 
 @Injectable()
 export class AuthenticationService {
@@ -56,11 +57,10 @@ export class AuthenticationService {
       throw new UnauthorizedException('Password does not match');
     }
     const accessToken = await this.jwtService.signAsync(
-      // 👈
       {
         sub: user.id,
         email: user.email,
-      },
+      } as ActiveUserData,
       {
         audience: this.jwtConfiguration.audience,
         issuer: this.jwtConfiguration.issuer,
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ActiveUser } from 'src/iam/decorators/active-user.decorator';
+import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UsersService) {}
 
   @Get('all-users')
-  findAll(): Promise<any> {
+  findAll(@ActiveUser() user: ActiveUserData): Promise<any> {
+    console.log(user);
     return this.userService.findAll();
   }
 }
