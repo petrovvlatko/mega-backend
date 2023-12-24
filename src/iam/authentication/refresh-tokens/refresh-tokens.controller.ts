7c58e3d2d036d8c132342aa6a82bc61b7306629e
V.Petrov <petrovvlatko93@gmail.com>
2023-12-24 13:32:48 -0500

    set up refresh token resource with more details, but running into errors

--- src/iam/authentication/refresh-tokens/refresh-tokens.controller.ts
+++ src/iam/authentication/refresh-tokens/refresh-tokens.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { RefreshTokensService } from './refresh-tokens.service';
 
 @Controller('refresh-tokens')
 export class RefreshTokensController {
   constructor(private readonly refreshTokensService: RefreshTokensService) {}
+
+  @Post('insert')
+  async insert(@Body() userId: number, tokenId: string) {
+    this.refreshTokensService.insert(userId, tokenId);
+  }
 }
--- src/iam/authentication/refresh-tokens/refresh-tokens.service.ts
+++ src/iam/authentication/refresh-tokens/refresh-tokens.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RefreshTokens } from './refresh-tokens.entity';
 
 @Injectable()
-export class RefreshTokensService {}
+export class RefreshTokensService {
+  constructor(
+    @InjectRepository(RefreshTokens)
+    private readonly refreshTokensRepository: Repository<RefreshTokens>,
+  ) {}
+
+  async insert(userId: number, tokenId: string) {
+    const payload = {
+      userId,
+      tokenId,
+    };
+    return this.refreshTokensRepository.save(payload);
+  }
+}
