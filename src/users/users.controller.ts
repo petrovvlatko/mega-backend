01c7e1c88750faf2daedc2391db90f25bce6e550
V.Petrov <petrovvlatko93@gmail.com>
2023-12-27 12:56:01 -0500

    saved roles

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -23,7 +23,7 @@ export class UserController {
     return this.userService.findAll();
   }
 
-  @Roles(Role.Admin)
+  @Roles(Role.Admin, Role.Basic)
   @Get('user-profile')
   findOne(@ActiveUser() user: ActiveUserData): Promise<any> {
     return this.userService.findOneById(user.sub);
