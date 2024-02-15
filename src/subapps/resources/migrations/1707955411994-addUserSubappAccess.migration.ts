6ab631756373646d149812edad14abc3b5e1d7b8
V.Petrov <petrovvlatko93@gmail.com>
2024-02-15 06:13:18 -0500

    added .migration to new migration filename.  curious why the nest cli did not do that in the first place

--- src/subapps/resources/migrations/1707955411994-addUserSubappAccess.migration.ts
+++ src/subapps/resources/migrations/1707955411994-addUserSubappAccess.migration.ts
@@ -7,7 +7,7 @@ export class AddUserSubappAccess1707955411994 implements MigrationInterface {
         "id" SERIAL NOT NULL PRIMARY KEY,
         "userId" character varying NOT NULL,
         "appId" character varying NOT NULL,
-        "subscription_tier" character varying NOT NULL,
+        "subscription_tier" character varying NOT NULL DEFAULT 'basic',
         "access_level" character varying NOT NULL DEFAULT 'basic',
         "created_at" TIMESTAMP NOT NULL DEFAULT now(),
         "updated_at" TIMESTAMP NOT NULL DEFAULT now()
