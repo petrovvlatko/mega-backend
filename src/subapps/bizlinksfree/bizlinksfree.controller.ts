29ae61f27791cf57bf19b76ffb54626737a59b0b
V.Petrov <petrovvlatko93@gmail.com>
2024-02-20 17:42:10 -0500

    aded resource for bizlinksfree and updated the auth to allow a GET request without any authorization or authentication

--- src/subapps/bizlinksfree/bizlinksfree.controller.ts
+++ src/subapps/bizlinksfree/bizlinksfree.controller.ts
@@ -2,8 +2,10 @@ import { Controller, Get } from '@nestjs/common';
 import { BizlinksfreeService } from './bizlinksfree.service';
 // import { CreateBizlinksfreeDto } from './dto/create-bizlinksfree.dto';
 // import { UpdateBizlinksfreeDto } from './dto/update-bizlinksfree.dto';
+import { Auth } from 'src/iam/decorators/auth.decorator';
+import { AuthType } from 'src/iam/enums/auth-type.enum';
 
-@Controller('bizlinksfree')
+@Controller('subapps/bizlinksfree')
 export class BizlinksfreeController {
   constructor(private readonly bizlinksfreeService: BizlinksfreeService) {}
 
@@ -11,7 +13,7 @@ export class BizlinksfreeController {
   // create(@Body() createBizlinksfreeDto: CreateBizlinksfreeDto) {
   //   return this.bizlinksfreeService.create(createBizlinksfreeDto);
   // }
-
+  @Auth(AuthType.None)
   @Get()
   async chillOut() {
     return { message: 'Chill the fuck out bro!' };
