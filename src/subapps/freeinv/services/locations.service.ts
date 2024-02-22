841f17d360ddea1c2f50da3903172cd21da24658
V.Petrov <petrovvlatko93@gmail.com>
2024-02-21 21:30:24 -0500

    more commenting with TODO's for the future

--- src/subapps/freeinv/services/locations.service.ts
+++ src/subapps/freeinv/services/locations.service.ts
@@ -1,3 +1,16 @@
+/*
+
+We NEED to eventually set up all the logic that allows for cascading deletions of rooms
+when a location is deleted, BUT we must remove the room id from the item entity without
+deleting the item itself.
+
+The goal is to make sure no Items are deleted unless the user manually deletes them,
+and they can just live in limbo without a room id until a one is appointed.
+
+UPDATE logic is next on the TODO list for these services.
+
+*/
+
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
