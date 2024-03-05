f332f8ebda6fcd93ff4e902fb6af5f9764c1d8d8
V.Petrov <petrovvlatko93@gmail.com>
2024-03-05 16:26:20 -0500

    updated links service and imports

--- src/subapps/mycuttingboard/resources/services/links.service.ts
+++ src/subapps/mycuttingboard/resources/services/links.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { MycuttingboardLinks } from '../../entities/mycuttingboardLinks.entity';
 @Injectable()
 export class LinksService {
+  constructor(
+    @InjectRepository(MycuttingboardLinks)
+    private readonly mycuttingboardLinksRepository: Repository<MycuttingboardLinks>,
+  ) {}
   async getMessage() {
     return 'Hello from links service!';
   }
