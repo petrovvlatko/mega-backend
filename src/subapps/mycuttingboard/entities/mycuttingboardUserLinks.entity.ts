6c52f1b547c9c5f78408f58f953ab43d6c4e7825
V.Petrov <petrovvlatko93@gmail.com>
2024-03-05 16:24:59 -0500

    updated names of tables, entities, and imports

--- src/subapps/mycuttingboard/entities/mycuttingboardUserLinks.entity.ts
+++ src/subapps/mycuttingboard/entities/mycuttingboardUserLinks.entity.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
-export class UserLinks {
+export class MycuttingboardUserLinks {
   @PrimaryGeneratedColumn()
   id: number;
 
