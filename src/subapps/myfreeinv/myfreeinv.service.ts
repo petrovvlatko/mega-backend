030ca9bb1ab18a9d3b8028fbcaa02698bf9228a3
V.Petrov <petrovvlatko93@gmail.com>
2024-03-14 08:29:44 -0400

    added TODO in myfreeinv service to clean up user data sent to the frontend

--- src/subapps/myfreeinv/myfreeinv.service.ts
+++ src/subapps/myfreeinv/myfreeinv.service.ts
@@ -12,13 +12,26 @@ export class FreeinvService {
     private readonly roomsService: RoomsService,
   ) {}
 
+  /*
+  TODO: refactor to send these lists without having to query each repository.
+
+    All the necessary data is in getAllLocationsWithRoomsAndItems() and we just
+    need to parse it all out into lists with the data we prefer
+
+    Locations - id, name, description, room quantity, item quantity
+    Rooms - id, name, description, location name, number of items
+    Items - id, name, description, room name, location name
+
+    We can return the same object with the added key/value pairs.
+    This should mean only minimal changes on the frontend will be needed
+      - Mostly just changing interfaces/types since all functions will still work
+  */
+
   async getAllUserData(userId: string) {
-    const completeData =
-      await this.locationsService.getAllLocationsWithRoomsAndItems(userId);
-    const locationsList =
-      await this.locationsService.findAllLocationsByUserId(userId);
-    const roomsList = await this.roomsService.findAllRoomsByUserId(userId);
-    const itemsList = await this.itemsService.findAllItemsByUserid(userId);
-    return { completeData, locationsList, roomsList, itemsList };
+    return await this.locationsService.getAllLocationsWithRoomsAndItems(userId);
+    // const locationsList =
+    //   await this.locationsService.findAllLocationsByUserId(userId);
+    // const roomsList = await this.roomsService.findAllRoomsByUserId(userId);
+    // const itemsList = await this.itemsService.findAllItemsByUserid(userId);
   }
 }
