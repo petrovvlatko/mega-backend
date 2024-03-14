c751baf82244727435481359ce3ae8a6800718b2
V.Petrov <petrovvlatko93@gmail.com>
2024-03-14 08:17:41 -0400

    changed all user data request to return locations, rooms, and items lists as well as a complete data object

--- src/subapps/myfreeinv/myfreeinv.controller.ts
+++ src/subapps/myfreeinv/myfreeinv.controller.ts
@@ -1,8 +1,5 @@
 import { Controller, Get, Req } from '@nestjs/common';
 
-import { LocationsService } from './resources/services/locations.service';
-import { RoomsService } from './resources/services/rooms.service';
-import { ItemsService } from './resources/services/items.service';
 import { FreeinvService } from './myfreeinv.service';
 
 import { Role } from 'src/users/enums/role.enum';
@@ -11,16 +8,11 @@ import { Roles } from 'src/iam/authorization/decorators/roles.decorator';
 @Roles(Role.Admin, Role.Basic)
 @Controller()
 export class FreeinvController {
-  constructor(
-    private readonly itemsService: ItemsService,
-    private readonly locationsService: LocationsService,
-    private readonly roomsService: RoomsService,
-    private readonly freeinvService: FreeinvService,
-  ) {}
+  constructor(private readonly freeinvService: FreeinvService) {}
 
   @Get('all-user-data')
-  async getAllLocationsWithRoomsAndItems(@Req() request) {
+  async getAllUserData(@Req() request) {
     const userId = request.user.sub;
-    return this.locationsService.getAllLocationsWithRoomsAndItems(userId);
+    return this.freeinvService.getAllUserData(userId);
   }
 }
--- src/subapps/myfreeinv/myfreeinv.service.ts
+++ src/subapps/myfreeinv/myfreeinv.service.ts
@@ -11,4 +11,14 @@ export class FreeinvService {
     private readonly locationsService: LocationsService,
     private readonly roomsService: RoomsService,
   ) {}
+
+  async getAllUserData(userId: string) {
+    const completeData =
+      await this.locationsService.getAllLocationsWithRoomsAndItems(userId);
+    const locationsList =
+      await this.locationsService.findAllLocationsByUserId(userId);
+    const roomsList = await this.roomsService.findAllRoomsByUserId(userId);
+    const itemsList = await this.itemsService.findAllItemsByUserid(userId);
+    return { completeData, locationsList, roomsList, itemsList };
+  }
 }
