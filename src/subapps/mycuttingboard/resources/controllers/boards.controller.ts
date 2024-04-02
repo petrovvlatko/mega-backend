1da238520605220c7cf1a56600d64755a43299ce
V.Petrov <petrovvlatko93@gmail.com>
2024-04-02 07:22:49 -0400

    updated user role access for mycuttingboard subapp

--- src/subapps/mycuttingboard/resources/controllers/boards.controller.ts
+++ src/subapps/mycuttingboard/resources/controllers/boards.controller.ts
@@ -5,25 +5,24 @@ import { AuthType } from 'src/iam/enums/auth-type.enum';
 import { Role } from 'src/users/enums/role.enum';
 import { Roles } from 'src/iam/authorization/decorators/roles.decorator';
 
-@Auth(AuthType.None)
+@Auth(AuthType.Bearer)
 @Controller('boards')
 export class BoardsController {
   constructor(private readonly boardsService: BoardsService) {}
 
+  @Auth(AuthType.None)
   @Get(':id')
   async getTestMessage(@Param('id') id: number) {
     return await this.boardsService.getBoardDataById(id);
   }
 
   @Roles(Role.Admin)
-  @Auth(AuthType.Bearer)
   @Post()
   async addBoardData(@Body() boardData) {
     return await this.boardsService.addBoardData(boardData);
   }
 
   @Roles(Role.Admin)
-  @Auth(AuthType.Bearer)
   @Delete(':id')
   async deleteBoardData(@Param('id') id: number) {
     return await this.boardsService.deleteBoardData(id);
