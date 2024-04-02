b679ba7480afbde6677d6ac6103a2b9cfebc5532
V.Petrov <petrovvlatko93@gmail.com>
2024-04-02 07:33:31 -0400

    removed redundancy in roles and auth.  auth is set to bearer for all endpoints in all subapp controllers from the top subapp controller.  individual endpoints can have auth removed and roles added.

--- src/subapps/myfreeinv/myfreeinv.controller.ts
+++ src/subapps/myfreeinv/myfreeinv.controller.ts
@@ -2,10 +2,6 @@ import { Controller, Get, Req } from '@nestjs/common';
 
 import { FreeinvService } from './myfreeinv.service';
 
-import { Role } from 'src/users/enums/role.enum';
-import { Roles } from 'src/iam/authorization/decorators/roles.decorator';
-
-@Roles(Role.Admin, Role.Basic)
 @Controller()
 export class FreeinvController {
   constructor(private readonly freeinvService: FreeinvService) {}
